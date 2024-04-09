58060a992d98bd40f5637bdf064eab8c946d0f8b
210352R <madurangahde.21@uom.lk>
2024-04-10 00:50:51 +0530

    update fireebase methodss part 01

--- server/firebaseStorage.py
+++ server/firebaseStorage.py
@@ -2,8 +2,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import storage
+from google.api_core.exceptions import DeadlineExceeded
+import time
 
-cred = credentials.Certificate("D:\OCR\pyTesseract\server\serviceAccountKey.json")
+
+cred = credentials.Certificate(r"D:\OCR\pyTesseract\server\serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 
@@ -11,14 +14,17 @@ db = firestore.client()
 
 # initialize storage
 
-bucket = storage.bucket("gs://ocr-test-3abc0.appspot.com")
+bucket = storage.bucket("ocr-test-3abc0.appspot.com")
 
 
 def add_data_to_firestore():
     print("DB : ", db)
     data = {"name": "Los Angeles", "state": "CA", "country": "USA"}
-    # Add a new doc in collection 'cities' with ID 'LA'
-    db.collection("cities").document("LA").set(data)
+
+    # Add data to firestore
+    db.collection("cities").add(data)
+
+    print("Added Sucesss ---------------------------------------------- ")
 
     return {"message": "Data added successfully"}
 
@@ -28,11 +34,55 @@ def add_image_to_storage():
 
     print("Call Method ---------------------------")
 
-    blob = bucket.blob("test01.jpg")
+    blob = bucket.blob("test03.jpg")
+
+    print("Blob : ", blob)
 
     # Upload the image file
-    blob.upload_from_filename("../test_images/image01.jpg")
+    blob.upload_from_filename(r"D:\OCR\pyTesseract\test_images\image03.jpg")
 
     # Get the public URL of the uploaded image
     blob.make_public()
+
+    print("URL : ", blob.public_url)
+
+    print("Added Sucesss ---------------------------------------------- ")
     return {"message": "Image added successfully"}
+
+
+# Create method for download image from storage
+def download_image_from_storage():
+    blob = bucket.blob("test03.jpg")
+
+    # Download the image
+    blob.download_to_filename(r"D:\OCR\pyTesseract\test_images\downloaded_image.jpg")
+
+    print("Downloaded Sucesss ---------------------------------------------- ")
+    return {"message": "Image downloaded successfully"}
+
+
+def add_data_to_firestore_with_retry():
+    retries = 3
+    for attempt in range(retries):
+        try:
+            data = {"name": "Los Ans", "state": "C", "country": "USoA"}
+            db.collection("cities").add(data)
+            print("Added Successfully")
+            return {"message": "Data added successfully"}
+        except DeadlineExceeded as e:
+            print(f"Attempt {attempt + 1} failed with DeadlineExceeded error: {e}")
+            if attempt < retries - 1:
+                # Wait for a short duration before retrying
+                time.sleep(1)
+            else:
+                # Max retries reached, raise the error
+                raise e
+
+
+# add_data_to_firestore_with_retry()
+
+# add_data_to_firestore()
+
+# add_image_to_storage()
+
+download_image_from_storage()
