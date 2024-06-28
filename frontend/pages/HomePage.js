df50d1a6b607ee8c9ddaac48254130c602a5c4a0
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-06-29 02:26:50 +0530

    update home page

--- frontend/pages/HomePage.js
+++ frontend/pages/HomePage.js
@@ -1,44 +1,60 @@
 // components/HomePage.js
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+
+// Import an image for the background
+const backgroundImage = require("../assets/redcar.jpg");
 
 const HomePage = () => {
   const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcomeText}>Welcome to BillsApp</Text>
-      <Text style={styles.subText}>
-        Your Predict sparepart lifetime from bill
-      </Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Camera")}
-      >
-        <Text style={styles.buttonText}>Get Started</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground source={backgroundImage} style={styles.background}>
+      <View style={styles.container}>
+        <Text style={styles.welcomeText}>Welcome to BillsApp</Text>
+        <Text style={styles.subText}>
+          Your Predict sparepart lifetime from bill
+        </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Camera")}
+        >
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
     width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f5f5f5",
-    padding: 20,
   },
   welcomeText: {
     fontSize: 28,
-    fontWeight: "bold",
-    color: "#333",
+
+    fontWeight: "900",
+    color: "white",
     marginBottom: 20,
   },
   subText: {
     fontSize: 16,
-    color: "#666",
+    color: "#F5F5F5",
     marginBottom: 40,
     textAlign: "center",
   },
